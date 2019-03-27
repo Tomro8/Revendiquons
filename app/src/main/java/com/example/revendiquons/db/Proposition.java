baddcd758da28bd83f1fc3736bcb0248d535f9fc
Tomro8 <38689975+Tomro8@users.noreply.github.com>
2019-03-27 18:15:38 +0100

    Pre-Branching commit for real Room shit2

--- app/src/main/java/com/example/revendiquons/db/Proposition.java
+++ app/src/main/java/com/example/revendiquons/db/Proposition.java
@@ -1,4 +1,62 @@
 package com.example.revendiquons.db;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Proposition {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int user_id;
+    private String title;
+    private String description;
+
+    public Proposition(int id, int user_id, String title, String description) {
+        this.id = id;
+        this.user_id = user_id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Proposition{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/example/revendiquons/db/PropositionDao.java
+++ app/src/main/java/com/example/revendiquons/db/PropositionDao.java
@@ -1,4 +1,23 @@
 package com.example.revendiquons.db;
 
-public class PropositionDao {
+import java.util.List;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import io.reactivex.Single;
+
+public interface PropositionDao {
+    @Insert
+    void insertAll(Proposition... Propositions);
+
+    @Update
+    void updatePropositions(Proposition... Propositions);
+
+    @Query("SELECT * FROM Proposition")
+    Single<List<Proposition>> getAll();
+
+    @Delete
+    public void deletePropositions(Proposition... Propositions);
 }
